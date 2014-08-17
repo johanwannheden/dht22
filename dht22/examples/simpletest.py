e4e3e3ac6811b676968983a15a5c93b8388953ab
Raspberry Pi Developer <johan.wannheden@gmail.com>
2014-08-17 19:15:16 +0000

    Updated sensor version from DHT22 -> AM 2302

--- dht22/examples/simpletest.py
+++ dht22/examples/simpletest.py
@@ -24,15 +24,15 @@ import Adafruit_DHT
 
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT22, or Adafruit_AM2302.
-sensor = Adafruit_DHT.DHT22
+sensor = Adafruit_DHT.AM2302
 
 # Example using a Beaglebone Black with DHT sensor
 # connected to pin P8_11.
-pin = 'P8_11'
+#pin = 'P8_11'
 
 # Example using a Raspberry Pi with DHT sensor
 # connected to pin 23.
-#pin = 23
+pin = 22
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
