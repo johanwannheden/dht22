0e7fa8d5bbef92cfac535933198b446d8fa85849
Raspberry Pi Developer <johan.wannheden@gmail.com>
2014-09-20 07:34:34 +0000

    Starting with cron job which writes to text file; modified py script

--- dht22/examples/AdafruitDHT.py
+++ dht22/examples/AdafruitDHT.py
@@ -45,6 +45,6 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).  
 # If this happens try again!
 if humidity is not None and temperature is not None:
-	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+	print '{0:0.1f} {1:0.1f}'.format(temperature, humidity)
 else:
 	print 'Failed to get reading. Try again!'
