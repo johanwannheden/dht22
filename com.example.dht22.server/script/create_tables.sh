6e567a295cd35851957715eb8324491d078461c5
Raspberry Pi Developer <johan.wannheden@gmail.com>
2014-10-14 21:38:27 +0200

    Corrected script to create DB; added PK

--- com.example.dht22.server/script/create_tables.sh
+++ com.example.dht22.server/script/create_tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-DDL='CREATE TABLE data (
+DDL="CREATE TABLE data (
 	id integer primary key autoincrement,
 	temperature number not null,
 	humidity number not null,
-	timestamp timestamp default (datetime(current_timestamp, "localtime"));'
+	stamp default (datetime(CURRENT_TIMESTAMP, 'localtime')));"
 sqlite3 ~/dht22.db "$DDL"
